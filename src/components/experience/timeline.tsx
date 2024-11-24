69a1884946e86c4723b467fcffffc1a762d17163
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2024-11-24 15:52:32 -0800

    ci: setup actions and format & lint

--- src/components/experience/timeline.tsx
+++ src/components/experience/timeline.tsx
@@ -3,11 +3,11 @@ import type { CAREER_STORY } from '@/constants'
 export function Timeline({ event }: { event: (typeof CAREER_STORY)[0] }) {
   return (
     <div>
-      <span className='text-foreground/70 font-mono text-xs sm:text-sm'>
+      <span className='font-mono text-xs text-foreground/70 sm:text-sm'>
         {event.year}
       </span>
       <h4 className='mt-2 text-sm font-medium sm:text-base'>{event.company}</h4>
-      <p className='text-foreground/70 text-xs sm:text-sm'>{event.role}</p>
+      <p className='text-xs text-foreground/70 sm:text-sm'>{event.role}</p>
     </div>
   )
 }
--- src/components/hero/badge.tsx
+++ src/components/hero/badge.tsx
@@ -3,7 +3,7 @@ import { MAIL } from '@/constants'
 export function Badge() {
   return (
     <a
-      className='text-foreground/80 relative inline-flex w-fit items-center gap-4 text-nowrap rounded-full border border-green-200 bg-gradient-to-br from-transparent to-green-200 px-3.5 py-1.5 text-center text-sm font-medium dark:border-green-950 dark:to-green-950'
+      className='relative inline-flex w-fit items-center gap-4 text-nowrap rounded-full border border-green-200 bg-gradient-to-br from-transparent to-green-200 px-3.5 py-1.5 text-center text-sm font-medium text-foreground/80 dark:border-green-950 dark:to-green-950'
       href={`mailto:${MAIL}`}
       rel='noopener noreferrer'
     >
